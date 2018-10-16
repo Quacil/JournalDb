69580794b758ce79b10bece7cfc1d7b43f37fa8e
Łukasz Kierepka <lukasz_kierepka@hotmail.com>
2018-10-16 19:40:15 +0200

    Added person table

---
+++ data/schema/person.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `person` (
+  `PersonId` int(11) NOT NULL AUTO_INCREMENT,
+  `Firstname` varchar(50) NOT NULL,
+  `Lastname` varchar(50) NOT NULL,
+  PRIMARY KEY (`PersonId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
